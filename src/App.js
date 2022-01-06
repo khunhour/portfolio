7949fbd0741f7a96d96388242fb5992a5910d74f
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-06 19:43:27 +0900

    created initial components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import Main from './Main'
 
 export default function App() {
   return (
     <div>
-      App
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <div>Logo</div>
+      <nav>
+        <li><a href="/">Home</a></li>
+        <li><a href='#About'>About Me</a></li>
+        <li><a href='#Projects'>Projects</a></li>
+      </nav>
+    </header>
+  )
+}
