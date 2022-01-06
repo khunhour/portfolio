61d926213da737c2d791ac4db8397e0009e404fe
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-06 20:12:36 +0900

    modify header

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "./Header.css";
 export default function Header() {
-  return (
-    <header>
-      <div>Logo</div>
-      <nav>
-        <li><a href="/">Home</a></li>
-        <li><a href='#About'>About Me</a></li>
-        <li><a href='#Projects'>Projects</a></li>
-      </nav>
-    </header>
-  )
+	return (
+		<header>
+			<h1>
+				khunhour<span>.dev</span>
+			</h1>
+			<nav>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="#About">About Me</a>
+				</li>
+				<li>
+					<a href="#Projects">Projects</a>
+				</li>
+			</nav>
+		</header>
+	);
 }
