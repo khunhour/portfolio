20d97388ae9465c3c6f46bbd99b4173a3869e4cf
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-08 03:27:37 +0900

    added a bit of footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 export default function Footer() {
-  return (
-    <div>
-      Footer
-    </div>
-  )
+	return (
+		<footer>
+			<div>Build By Khunhour</div>
+			<div>
+				<a href="https://github.com/khunhour/portfolio">
+					<FaGithub color="var(--accent-color)" />
+				</a>
+			</div>
+		</footer>
+	);
 }
