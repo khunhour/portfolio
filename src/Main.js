50d0d639435277e10b2f1f4ddf004e90ff2e3e69
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-07 20:35:54 +0900

    added button and text to hero

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import About from "./components/About";
-import Hero from "./components/Hero";
+import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 export default function Main() {
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hero.css";
+export default function Hero() {
+	return (
+		<section>
+			<div className="mono">Hi there, My name is</div>
+			<h2>Khunhour Morm.</h2>
+			<h1 className="accent">Front-End Web Developer.</h1>
+			<h1>I build things and make them pretty on the Internet.</h1>
+			<button>Resume</button>
+		</section>
+	);
+}
