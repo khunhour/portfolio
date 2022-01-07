8e363d17fb36884daf0aecc92042d2ab95960fbc
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-07 21:30:38 +0900

    added description in about me section

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,14 @@ export default function About() {
 	return (
 		<section id="about">
 			<h2>About Me</h2>
-			<p>description</p>
+			<div>
+				Hello! My name is <span>Khunhour</span> and I enjoy long walks
+				on the beach and JavaScript. Born in Cambodia, lost in Japan. I
+				am a self-taught web developer on a path to become a Full Stack
+				software engineer. I've always like building things and when I
+				started touching on HTML and CSS, I knew that <span>this</span>{" "}
+				is what I want to do in my career.
+			</div>
 		</section>
 	);
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import "./Hero.css";
 export default function Hero() {
 	return (
 		<section id="home">
-			<div className="mono">Hi there, My name is</div>
+			<div className="mono">Hi there! My name is</div>
 			<div className="name">Khunhour Morm.</div>
 			<h1 className="accent">Front-End Web Developer.</h1>
 			<h1>I build things and make them pretty on the Internet.</h1>
