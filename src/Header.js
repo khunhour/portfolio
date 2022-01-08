1d5a4da37de5f4488280ee1cc9ffe9e550ce8c63
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-08 19:57:29 +0900

    added menu button and slide links

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
-import { MdMenu } from "react-icons/md";
+import { MdMenu, MdClose } from "react-icons/md";
 
 export default function Header() {
+	const [linksHidden, setLinksHidden] = useState(false);
+	const toggleMenuBtn = () => {
+		setLinksHidden(!linksHidden);
+	};
+	const className = linksHidden ? "" : "active";
+
 	return (
 		<header>
 			<h1>
 				khunhour<span>.dev</span>
 			</h1>
 			<nav>
-				<li>
-					<a href="#home">Home</a>
-				</li>
-				<li>
-					<a href="#about">About</a>
-				</li>
-				<li>
-					<a href="#skills">Skills</a>
-				</li>
-				<li>
-					<a href="#projects">Projects</a>
-				</li>
-				<li>
-					<a href="#contacts">Contacts</a>
-				</li>
+				<ul className={className}>
+					<li>
+						<a href="#home">Home</a>
+					</li>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#skills">Skills</a>
+					</li>
+					<li>
+						<a href="#projects">Projects</a>
+					</li>
+					<li>
+						<a href="#contacts">Contacts</a>
+					</li>
+				</ul>
+				<div className="menu-btn" onClick={toggleMenuBtn}>
+					{linksHidden ? (
+						<MdMenu size={"1.8em"} />
+					) : (
+						<MdClose size={"1.8em"} />
+					)}
+				</div>
 			</nav>
-			{/* <MdMenu size={"1.8em"} /> */}
 		</header>
 	);
 }
