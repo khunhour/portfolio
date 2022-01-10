88ff39c6073a9d7e999ea6da642a3e3c31d9465c
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-10 20:36:54 +0900

    edit the about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,30 +5,22 @@ export default function About() {
 		<section id="about">
 			<h2>About Me</h2>
 			<p>
-				Hello! My name is <span>Khunhour</span> and I enjoy long walks
-				on the beach and JavaScript. I am a self-taught web developer 
-				on a path to become a Full Stack software engineer. Ever since 
-				I could remember, I've always like building things and when I
-				started touching on HTML and CSS, I knew that <span>this</span>{" "}
-				is what I want to do in my career.
+				Hello! My name is <span>Khunhour</span> and I am a self-taught
+				web developer based in Japan. Ever since I could remember, I've
+				always like building things and when I started touching on HTML
+				and CSS, I knew that <span>this</span> is what I want to do in
+				my career.
 			</p>
 			<p>
-				Since then, I've been learning more about Front-End technologies like 
-				<span>React</span> and use it to build exciting projects as well as 
-				contribute to open-source projects.
+				Since then, I've been learning more about Front-End technologies
+				like <span>React</span> and use it to build exciting projects as
+				well as contribute as much as I can to open-source projects.
 			</p>
 			<p>
-				...and I'm not stopping there. Here are some of the technologies, 
-				I plan to learn and improve this year: 
+				Currently, I'm planning to learn about the <span>Back-End</span>{" "}
+				side of web development, particularly with technologies like{" "}
+				<span>Firebase</span> and <span>Node.js</span>.
 			</p>
-			<ul className="yellow-bullet-lists">
-				<li>Redux</li>
-				<li>Node.js</li>
-				<li>MongoDB</li>
-				<li>Express</li>
-				<li>Styled-Components</li>
-				<li>Figma</li>
-			</ul>
 		</section>
 	);
 }
