01954a4c8435ccb74c2a1ad4a99153433444e41b
Khunhour Morm <khonhourmorm@gmail.com>
2022-01-10 16:45:13 +0900

    rearrange skills

--- src/components/Skills/Skills_constant.js
+++ src/components/Skills/Skills_constant.js
@@ -1,19 +1,11 @@
 export const skills = [
 	{
-		name: "HTML",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-	},
-	{
-		name: "CSS",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+		name: "React",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
 	},
 	{
 		name: "JavaScript",
 		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-	},
-	{
-		name: "React",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
 	},
 	{
 		name: "Jest",
@@ -30,5 +22,13 @@ export const skills = [
 	{
 		name: "Webpack",
 		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg",
+	},
+	{
+		name: "HTML",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+	},
+	{
+		name: "CSS",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
 	},
 ];
