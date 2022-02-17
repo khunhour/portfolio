ec9b6084e0585cdce840b6c3a78116ff30c9bda4
khunhour <khonhourmorm@gmail.com>
2022-02-17 17:40:22 +0900

    added japanese text into hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,14 @@ import "./Hero.css";
 export default function Hero({ handleViewResume }) {
 	return (
 		<section id="home">
-			<div className="mono">Hi there! My name is</div>
-			<div className="name">Khunhour Morm.</div>
-			<h1 className="accent">Front-End Web Developer.</h1>
-			<h1>I build things and make them pretty on the Internet.</h1>
+			{/* <div className="mono">Hi there! My name is</div> */}
+			<div className="mono">こんにちは！私の名前は</div>
+			<div className="name">Khunhour Mormです.</div>
+			{/* <h1 className="accent">Front-End Web Developer.</h1> */}
+			<h1 className="accent">フロントエンドエンジニア</h1>
 
+			{/* <h1>I build things and make them pretty on the Internet.</h1> */}
+			<h1>私はインターネット上で物を作るのが好きです。</h1>
 			<a
 				href="#resume"
 				className="link-button"
