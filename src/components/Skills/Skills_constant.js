ebea07c2a7d8fdcbf31a90d418e515deb0a3b8d0
Khunhour Morm <khonhourmorm@gmail.com>
2022-02-26 18:20:58 +0900

    added material UI as skill

--- src/components/Skills/Skills_constant.js
+++ src/components/Skills/Skills_constant.js
@@ -1,42 +1,46 @@
 export const skills = [
-	// {
-	// 	name: "TypeScript",
-	// 	src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-	// },
-	{
-		name: "React",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-	},
-	// {
-	// 	name: "Redux",
-	// 	src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
-	// },
-	{
-		name: "JavaScript",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-	},
-	{
-		name: "Jest",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
-	},
-	{
-		name: "Git",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
-	},
-	{
-		name: "Npm",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg",
-	},
-	{
-		name: "Webpack",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg",
-	},
-	{
-		name: "HTML",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-	},
-	{
-		name: "CSS",
-		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-	},
+  // {
+  // 	name: "TypeScript",
+  // 	src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+  // },
+  {
+    name: "React",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  // {
+  // 	name: "Redux",
+  // 	src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+  // },
+  {
+    name: "JavaScript",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "Jest",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+  },
+  {
+    name: "Git",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+  },
+  {
+    name: "Npm",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg",
+  },
+  {
+    name: "Webpack",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg",
+  },
+  {
+    name: "HTML",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    name: "Material UI",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+  },
 ];
