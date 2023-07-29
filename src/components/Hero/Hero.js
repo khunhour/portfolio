5fd449ec550b692eff5745248aecd2d1d884cf94
Khunhour Morm <khonhourmorm@gmail.com>
2023-07-30 03:58:30 +0900

    revert node version

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ export default function Hero({ handleViewResume }) {
 		<section id="home">
 			<div className="mono">Hi there! My name is</div>
 			<div className="name">Khunhour Morm.</div>
-			<h1 className="accent">Software Developer.</h1>
+			<h1 className="accent">Software Engineer.</h1>
 			<h1>I build things and make them pretty on the Internet.</h1>
 
 			<a
