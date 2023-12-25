95e2c981d3958c2b6834bb4c2c0854769c7fbe0e
Khunhour Morm <khonhourmorm@gmail.com>
2023-12-26 01:51:08 +0900

    rename with capital letter E

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { experience } from "./experienceConstant";
 import ExperienceCard from "./ExperienceCard";
 import "./Experience.css";
+import { experiences } from "./ExperienceConstant";
 
 export default function Experience() {
 	return (
 		<section id="experience">
 			<h2>Experience</h2>
 			<div className="experience-container">
-				{experience.map((experience, index) => (
+				{experiences.map((experience, index) => (
 					<ExperienceCard experience={experience} key={index} />
 				))}
 			</div>
--- src/components/Experience/experienceConstant.js
+++ src/components/Experience/experienceConstant.js
@@ -1,4 +1,4 @@
-export const experience = [
+export const experiences = [
 	{
 		title: "Software Engineer",
 		company: "MoneyForward",
