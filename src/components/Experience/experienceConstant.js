fa965007d291015335e8cf9092edbf8d71b5ef61
Khunhour Morm <khonhourmorm@gmail.com>
2023-12-26 01:47:09 +0900

    update experience description

---
+++ src/components/Experience/experienceConstant.js
@@ -0,0 +1,12 @@
+export const experience = [
+	{
+		title: "Software Engineer",
+		company: "MoneyForward",
+		location: "Tokyo, Japan",
+		date: "April 2023 - Present",
+		descriptions: [
+			"Developed microservices for clould accounting including creating new APIs using GRPC",
+			"Was responsible for monitoring, maintaining, and incident handling for existing microservices using technologies like Datadog, Rollbar, etc",
+		],
+	},
+];
